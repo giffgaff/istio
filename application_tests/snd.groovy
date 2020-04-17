2d969265ffbd5b2da6ec54df9aec0fefd57b1029
Matias Costa Tirado <matias@giffgaff.co.uk>
2020-04-17 13:23:04 +0100

    Add ingresses to snd

--- application_tests/snd.groovy
+++ application_tests/snd.groovy
@@ -1,6 +1,7 @@
 def main(graph) {
     sh script: '''
-      istioctl manifest apply -f profiles/snd.yml
+      istioctl manifest apply -f profiles/snd/snd.yml
+      kubectl apply -f profiles/snd/ingress.yml
     '''
 }
 return this
